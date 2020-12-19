275d306812a3b2cb3aa2dc70cd92d7ec6b0a9bbf
maelleeveno <maelle.eveno@outlook.com>
2020-12-19 23:30:46 +0100

    style updates + find solution to fix the duplicate slide issue causing slider display bug

--- src/components/Motion/airfrance.jsx
+++ src/components/Motion/airfrance.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import { Grid, Icon } from 'semantic-ui-react';
 import Swiper from 'react-id-swiper';
+import VideoPlayer from '../VideoPlayer';
 import './motion.scss';
 
-import af1 from '../../assets/images/motion/air-france/AIRFRANCE_1.gif';
-import af2 from '../../assets/images/motion/air-france/AIRFRANCE_2.gif';
-import af3 from '../../assets/images/motion/air-france/AIRFRANCE_3.gif';
-import af4 from '../../assets/images/motion/air-france/AIRFRANCE_4.gif';
+import af1Mp4 from '../../assets/images/motion/air-france/AIRFRANCE_1.mp4';
+import af2Mp4 from '../../assets/images/motion/air-france/AIRFRANCE_2.mp4';
+import af3Mp4 from '../../assets/images/motion/air-france/AIRFRANCE_3.mp4';
+import af4Mp4 from '../../assets/images/motion/air-france/AIRFRANCE_4.mp4';
+
+import af1Webm from '../../assets/images/motion/air-france/AIRFRANCE_1.mp4';
+import af2Webm from '../../assets/images/motion/air-france/AIRFRANCE_2.mp4';
+import af3Webm from '../../assets/images/motion/air-france/AIRFRANCE_3.mp4';
+import af4Webm from '../../assets/images/motion/air-france/AIRFRANCE_4.mp4';
 
 const AirFrance = () => {
 
-	const params = {
+    const params = {
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
@@ -19,8 +25,9 @@ const AirFrance = () => {
 		loop: true,
 		spaceBetween: 20,
 		centeredSlides: true,
-		slidesPerView: 'auto',
-	};
+        slidesPerView: 'auto',
+        rebuildOnUpdate: true,
+    };
 
 	const [showDesc, setShowDesc] = useState('hide');
 	const [arrowDir, setArrowDir] = useState('arrow down');
@@ -40,11 +47,11 @@ const AirFrance = () => {
             <Grid className="motion-subsection">
                 <Grid.Row columns={2}>
                     <Grid.Column className="carousel">
-                        <Swiper {...params}>
-                            <img className="gif swiper-slide" src={af1} alt="" />
-                            <img className="gif swiper-slide" src={af2} alt="" />
-                            <img className="gif swiper-slide" src={af3} alt="" />
-                            <img className="gif swiper-slide" src={af4} alt="" />
+                        <Swiper {...params} shouldSwiperUpdate>
+                            <VideoPlayer className="gif swiper-slide" videoMp4={af1Mp4} videoWebm={af1Webm} />
+                            <VideoPlayer className="gif swiper-slide" videoMp4={af2Mp4} videoWebm={af2Webm} />
+                            <VideoPlayer className="gif swiper-slide" videoMp4={af3Mp4} videoWebm={af3Webm} />
+                            <VideoPlayer className="gif swiper-slide" videoMp4={af4Mp4} videoWebm={af4Webm} />
                         </Swiper>
                     </Grid.Column>
                     <Grid.Column  className="project-desc">
