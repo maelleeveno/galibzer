113ec968d6c48eb210688647f58b0c6ffbad9713
maelleeveno <maelle.eveno@outlook.com>
2020-12-20 20:57:40 +0100

    setting infinite scroll on motions

--- src/components/Motion/index.jsx
+++ src/components/Motion/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Layout from '../Layout';
 import Title from '../Title';
 import './motion.scss';
@@ -14,21 +15,53 @@ import DeParis from './deparis';
 import ChicagoFire from './chicagofire';
 
 const Motion = () => {
+
+	const [state, setState] = useState({ items: [<Hennessy/>] });
+	const [extraComponents] = useState([
+		<AirFrance/>,
+		<BigInJapan/>,
+		<Orangina/>,
+		<Peugeot/>,
+		<DeParis/>,
+		<Leclerc/>,
+		<ChicagoFire/> 
+	]);
+	const [count, setCount] = useState(0);
+	const [hasMore, setHasMore] = useState(true);
+	
+	const fetchMoreData = () => {
+		if(state.items.length >=20 ) {
+			setHasMore(false);
+			return;
+		}
+		setTimeout( () => {
+			setState({
+				items: state.items.concat([extraComponents[count]])
+			});
+			setCount(count + 1);
+		}, 500);
+	}
+
 	return (
 		<Layout>
 			<Title title="Motion | Pacôme Gabrillagues"/>
 			<section id="motion">
-				<Hennessy/>
-				<AirFrance/>
-				<BigInJapan/>
-				<Orangina/>
-				<Peugeot/>
-				<DeParis/>
-				<Leclerc/>
-				<ChicagoFire/>
+				<InfiniteScroll
+					dataLength={state.items.length}
+					next={fetchMoreData}
+					hasMore={hasMore}
+					loader={<p className="loading">Loading...</p>}
+					endMessage={
+						<p className="end-message">Yay! Nothing more to see for now :)</p>
+					}
+					className="no-y-scroll"
+				>
+					{state.items.map( (i, index) => i)}				
+				</InfiniteScroll>
 			</section>
 		</Layout>
 	);
+	
 }
 
 export default Motion;
