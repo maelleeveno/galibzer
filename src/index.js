6107d8985c157c13009ebbf47a3e24102c6c4b66
maelleeveno <maelle.eveno@outlook.com>
2021-01-23 14:57:10 +0100

    custom tarteaucitron script and style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/assets/styles/global.scss';
+import '../src/assets/styles/tarteaucitron.scss';
 import '../src/assets/styles/theme.scss';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
