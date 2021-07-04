59ba0d9afdfc3d376d653b1f62e509dea04e1fa9
maelleeveno <maelle.eveno@outlook.com>
2021-07-04 17:24:17 +0200

    still not working by refreshing compo page with tarteaucitron / back to iframe integration

--- src/components/Compo/index.jsx
+++ src/components/Compo/index.jsx
@@ -9,8 +9,8 @@ const Compo = () => {
 		<Layout>
 			<Title title="Compo | Pacôme Gabrillagues"/>
 			<section id="compo">
-				<div className="youtube_player" videoid="nIL95lspiyg" width="100%" height="329"></div>
-				{/* <iframe className="youtube-video" title="Compo" width="800" height="329" src="https://www.youtube.com/embed/nIL95lspiyg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+				{/* <div className="youtube_player" videoid="nIL95lspiyg" width="100%" height="329"></div> */}
+				<iframe className="youtube-video" title="Compo" width="800" height="329" src="https://www.youtube.com/embed/nIL95lspiyg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				<div className="desc">
 					<p>AE VFX compositing showreel featuring a compilation of some recent works.</p>
 				</div>
